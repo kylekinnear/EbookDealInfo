0945bddb014318b7119e784a0937fa56a1a898d4
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-08-30 12:50:59 -0400

    scraper

--- lib/deal_scraper.rb
+++ lib/deal_scraper.rb
@@ -5,9 +5,22 @@ class DealScraper
 
   def scrape #this method scrapes the deals page, then for each item on that page, instantiates a new book with the relevant info; are we passing a hash or attributes?
     deals = Nokogiri::HTML(open("https://www.reddit.com/r/ebookdeals/new/"))
+    deals.search("div.link").each do |post|
+      if post.search("p.title").text.size > 0
+        if post.search("p.title").text.include?(";")
+          #semicolon separator slicing
+        else
+          #by slicing
+        end
+      end
+      #reject the entry if this is blank as a failsafe
+      #figure out which format this post uses
+      #slice the string appropriately
   end
 
 end
+
+
 
 
 "div#siteTable .sitetable linklisting" #wrapper
