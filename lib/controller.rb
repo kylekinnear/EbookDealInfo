aa20d43f90f63b6cf3c1d47dacc107ae7c5e125f
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-08-30 14:22:07 -0400

    menus

--- lib/controller.rb
+++ lib/controller.rb
@@ -10,16 +10,18 @@ class Controller
 
   def welcome
     #intro message
-    puts "Welcome to the Ebook Daily Deal Info Getter"
-    puts "Getting today's deals"
+    puts "Welcome to the Ebook Recent Deal Info Getter"
+    puts "Getting the latest deals"
     puts "-----------------------------------------"
-    #start scraping
+    DealScraper.new #call DealScraper, and through it instantiate books and call info_scraper on the books
     self.menu #main menu
   end
 
   def menu
     #the main menu
-    puts "A list of today's deals:"
+    puts "A list of the latest deals:"
+    Book.all.each_with_index(1) do |book, index|
+      puts "#{index}. #{book.title} - #{book.author} - #{book.genre}"
     #uses formatter to format the list
     #allows user to select an item for more information or exit the program
     #loop to handle odd inputs
