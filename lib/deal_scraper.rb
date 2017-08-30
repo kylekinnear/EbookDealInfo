1516d4d060530cf9c7334779fc6fbe874dd46c4c
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-08-30 11:28:36 -0400

    deal scraper stub

--- lib/deal_scraper.rb
+++ lib/deal_scraper.rb
@@ -2,7 +2,7 @@
 
 class DealScraper
 
-  def scrape #this method scrapes the deals page, then for each item on that page, instantiates a new book with the relevant info
+  def scrape #this method scrapes the deals page, then for each item on that page, instantiates a new book with the relevant info; are we passing a hash or attributes?
     deals = Nokogiri::HTML(open("https://www.amazon.com/s/ref=s9_acsd_hps_ft_clnk_r?node=6165851011"))
   end
 
