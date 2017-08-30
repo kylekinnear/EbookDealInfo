5557e77ca5c1efafc5e925ff380e823d9db7b6ea
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-08-30 13:11:12 -0400

    book v0.1 complete

--- lib/book.rb
+++ lib/book.rb
@@ -2,17 +2,18 @@
 require "EbookDealInfo"
 
 class Book
-  attr_accessor :title, :author, :genre, :series, :goodreads_rating, :goodreads_rates, :blurb, :price
+  attr_accessor :author, :title, :price, :genre, :series, :goodreads_rating, :goodreads_rates, :blurb
   @@all = [] #collection to push all new books into
 
-  def initialize(arguments)
+  def initialize(author, title, price)
     @@all << self #push new books
-    #do we want to set the attributes here or in create?
+    @author = author
+    @title = title
+    @price = price
   end
 
-  def self.create(item_from_deals_scrape_RENAME) #deals scrape will pass in hash with title, author, and price
-    self.new #more efficient to pass in data here and do it in initialize or set below?
-    #potential set attributes
+  def self.create(author, title, price) #deals scrape will pass in hash with title, author, and price
+    self.new(author, title, price)
   end
 
   def self.all #we'll use this to grab the collection, which the goodreads scraper will use
