c975a5ca92f5b62591b3a9e09004c4dbda10791a
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-08-30 11:19:23 -0400

    book.rb architecture

---
+++ lib/book.rb
@@ -0,0 +1,22 @@
+#this class holds the data for each book
+#requirements?
+
+class Book
+  attr_accessor :title, :author, :genre, :series, :amz_rating, :amz_rates, :goodreads_rating, :goodreads_rates, :blurb, :price
+  @@all = [] #collection to push all new books into
+
+  def initialize(arguments)
+    @@all << self #push new books
+    #do we want to set the attributes here or in create?
+  end
+
+  def self.create(item_from_deals_scrape_RENAME) #deals scrape will pass in hash with title, author, amz rating/rates, and price
+    self.new #more efficient to pass in data here and do it in initialize or set below?
+    #potential set attributes
+  end
+
+  def self.all #we'll use this to grab the collection, which the goodreads scraper will use
+    @@all
+  end
+
+end
