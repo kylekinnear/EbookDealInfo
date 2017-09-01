8dd5726e6e520a50dc8c55ac5431254fba5cfd96
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-09-01 13:25:10 -0400

    removed comments, fixed missing genre 2

--- lib/info_scraper.rb
+++ lib/info_scraper.rb
@@ -1,4 +1,4 @@
-#this is the goodreads scraper
+""#this is the goodreads scraper
 require_relative "EbookDealInfo"
 
 class InfoScraper
@@ -11,13 +11,12 @@ class InfoScraper
       determinant = search_page.css("span.minirating").map.with_index {|i,index| [index, i.text.strip.slice(/\s(\d|,)+/).strip.gsub(",","").to_i]}.sort! {|x,y| x[1].to_i <=> y[1].to_i}.last #the search result with the most rates (and presumably most legitimate) is an array [result_index, #rates]
       item_page = Nokogiri::HTML(open("https://goodreads.com/#{search_page.css("table a.bookTitle")[determinant[0]].attribute("href").value}",'User-Agent' => 'Ruby').read)
       book.author = item_page.search("div#bookAuthors.stacked span :not(.greyText) :not(.smallText)").text #gets the complete author name since reddit might not provide it
-      #how do we handle no_series, and can we tidy the code?
       book.title = item_page.search("h1#bookTitle.bookTitle").text.slice(/^(\n).+(\S\n)/).strip #goodreads provides better titles
       book.series = item_page.search("h1#bookTitle.bookTitle :first-child").text.strip.gsub(/[()]/, "") #provides series
       book.rating = item_page.search("span.average").text #average rating
       book.rates = item_page.search("span.votes.value-title").text.strip #number of ratings
       #blurb needs work
-      book.blurb = item_page.xpath('//span[starts-with(@id, "freeText")]')[1].text#.gsub(/\s+/, " ") #grab the blurb
+      book.blurb = item_page.xpath('//span[starts-with(@id, "freeText")]')[1].text#grab the blurb
       #we will scrape the top two genre entries, but when we want to check if one is a more specific form of the other
       if item_page.search("div.bigBoxContent div.elementList div.left").empty?
         book.genre_one = "No genre listed"
@@ -26,9 +25,13 @@ class InfoScraper
         genre_one = item_page.search("div.bigBoxContent div.elementList div.left")[0].text.split("\n").map {|i| i.strip if i.strip.size > 0}.reject {|i| i==nil} # turn the first genre into a stripped array of actual content
         book.genre_one = ""
         genre_one.each {|i| book.genre_one << "#{i} "}
-        genre_two = item_page.search("div.bigBoxContent div.elementList div.left")[1].text.split("\n").map {|i| i.strip if i.strip.size > 0}.reject {|i| i==nil}
-        book.genre_two = ""
-        genre_two.each {|i| book.genre_two << "#{i} "}
+        if item_page.search("div.bigBoxContent div.elementList div.left")[1] != nil
+          genre_two = item_page.search("div.bigBoxContent div.elementList div.left")[1].text.split("\n").map {|i| i.strip if i.strip.size > 0}.reject {|i| i==nil}
+          book.genre_two = ""
+          genre_two.each {|i| book.genre_two << "#{i} "}
+        else
+          book.genre_two = ""
+        end
       end
     else #instead of a raising a nobook error that will break the looping, let's flag the book as incomplete and not display it at the end
       book.completable = false
