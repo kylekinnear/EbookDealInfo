1244551cd62d5e4299bdcd72fe3adfdadfc207ee
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-09-02 13:27:46 -0400

    tweaks to DealScraper

--- lib/ebookdealinfo/deal_scraper.rb
+++ lib/ebookdealinfo/deal_scraper.rb
@@ -13,15 +13,16 @@ class DealScraper
           title = post.search("p.title").text.split(";")[1].strip
           post.search("p.title").text.split(";")[2] != nil ? price = post.search("p.title").text.split(";")[2].strip : price = ""
           Book.create(author, title, price)
-          puts "Loaded book ##{index+1} of 25"
+          puts "Loaded book ##{index+1} of #{deals.search("div.link").size}"
         else #for posts formatted "~Title~ by ~author~ (~Price~)"
           if post.search("p.title").text.slice(/[,.-[ ]]([Bb]y)/) != nil #ignore wacky formatted posts
             author = post.search("p.title").text.split("by")[1].strip.slice(/\A[^(,$\/]+/).split(". Kindle")[0].split("-- Kindle")[0].strip
             title = post.search("p.title").text.split("by").first.gsub(/[(].+[)]/,"").gsub(/\W+\z/, "").strip
             post.search("p.title").text.slice(/[$]\d+[.]\d+/) != nil ? price = post.search("p.title").text.slice(/[$]\d+[.]\d+/).strip : price = ""
             Book.create(author, title, price)
-            puts "Loaded book ##{index+1} of 25"
+            puts "Loaded book ##{index+1} of #{deals.search("div.link").size}"
           else
+            puts "Unable to load book ##{index+1} of #{deals.search("div.link").size}. Probably a bad post name."
             Book.create("","","",0)
           end
         end
