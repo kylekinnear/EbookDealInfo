31cd885dfa812b9c4ffae4d8848e46eeb39a6584
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-09-02 10:07:20 -0400

    better handling for free books

--- lib/ebookdealinfo/deal_scraper.rb
+++ lib/ebookdealinfo/deal_scraper.rb
@@ -10,16 +10,15 @@ class DealScraper
       if post.search("p.title").text.size > 0
         if post.search("p.title").text.include?(";") #for posts formatted "#~Author~; ~Title; ~Price~"
           author = post.search("p.title").text.split(";")[0].strip
-          #.search("p.title a").attribute("href").value
           title = post.search("p.title").text.split(";")[1].strip
-          price = post.search("p.title").text.split(";")[2].strip #can't handle free books
+          post.search("p.title").text.split(";")[2] != nil ? price = post.search("p.title").text.split(";")[2].strip : price = ""
           Book.create(author, title, price)
           puts "Loaded book ##{index+1} of 25"
         else #for posts formatted "~Title~ by ~author~ (~Price~)"
           if post.search("p.title").text.slice(/\s([Bb]y)/) != nil #ignore wacky formatted posts
             author = post.search("p.title").text.split("by")[1].slice(/\A[^(,$\/]+/).split(". Kindle")[0].split("-- Kindle")[0].strip
-            title = post.search("p.title").text.split("by").first.gsub(/\W+\z/, "").strip
-            price = post.search("p.title").text.slice(/[$]\d+[.]\d+/).strip #can't handle free books
+            title = post.search("p.title").text.split("by").first.gsub(/[(].+[)]/,"").gsub(/\W+\z/, "").strip
+            post.search("p.title").text.slice(/[$]\d+[.]\d+/) != nil ? price = post.search("p.title").text.slice(/[$]\d+[.]\d+/).strip : price = ""
             Book.create(author, title, price)
             puts "Loaded book ##{index+1} of 25"
           else
