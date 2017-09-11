5381391bde6c5f7b2b083fcd4e0cb6cb72844d70
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-09-11 15:03:33 -0400

    fixed 'By' breaking the reddit scrape; a book with a 'by' in the title will now default to unloadable

--- lib/ebookdealinfo/deal_scraper.rb
+++ lib/ebookdealinfo/deal_scraper.rb
@@ -16,11 +16,16 @@ class DealScraper
           puts "Loaded book ##{index+1} of #{deals.search("div.link").size}"
         else #for posts formatted "~Title~ by ~author~ (~Price~)"
           if post.search("p.title").text.slice(/[,.-[ ]]([Bb]y)/) != nil #ignore wacky formatted posts
-            author = post.search("p.title").text.split("by")[1].strip.slice(/\A[^(,$\/]+/).split(". Kindle")[0].split("-- Kindle")[0].strip
-            title = post.search("p.title").text.split("by").first.gsub(/[(].+[)]/,"").gsub(/\W+\z/, "").strip
-            post.search("p.title").text.slice(/[$]\d+[.]\d+/) != nil ? price = post.search("p.title").text.slice(/[$]\d+[.]\d+/).strip : price = ""
-            Book.create(author, title, price)
-            puts "Loaded book ##{index+1} of #{deals.search("div.link").size}"
+            if post.search("p.title").text.scan(/(\s[Bb][Yy]\s)/).size > 1 #count instances of "by"; if the title includes a "by" we can't parse it
+              puts "Unable to load book ##{index+1} of #{deals.search("div.link").size}. Probably a bad post name."
+              Book.create("","","",0)
+            else
+              author = post.search("p.title").text.gsub(/(\s[Bb][Yy]\s)/, " by ").split("by")[1].strip.slice(/\A[^(,$\/]+/).split(". Kindle")[0].split("-- Kindle")[0].strip
+              title = post.search("p.title").text.gsub(/(\s[Bb][Yy]\s)/, " by ").split("by").first.gsub(/[(].+[)]/,"").gsub(/\W+\z/, "").strip
+              post.search("p.title").text.slice(/[$]\d+[.]\d+/) != nil ? price = post.search("p.title").text.slice(/[$]\d+[.]\d+/).strip : price = ""
+              Book.create(author, title, price)
+              puts "Loaded book ##{index+1} of #{deals.search("div.link").size}"
+            end
           else
             puts "Unable to load book ##{index+1} of #{deals.search("div.link").size}. Probably a bad post name."
             Book.create("","","",0)
